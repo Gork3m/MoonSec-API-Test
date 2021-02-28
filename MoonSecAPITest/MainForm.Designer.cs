28d33fed2af77d202bee99839efdcae0066e7ac3
Görkem / NT <67007893+Gork3m@users.noreply.github.com>
2021-02-28 20:22:26 +0300

    Add files via upload

---
+++ MoonSecAPITest/MainForm.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace MoonSecAPITest
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.RadioButton radioButton7;
+		private System.Windows.Forms.RadioButton radioButton6;
+		private System.Windows.Forms.RadioButton radioButton5;
+		private System.Windows.Forms.RadioButton radioButton4;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.radioButton7 = new System.Windows.Forms.RadioButton();
+			this.radioButton6 = new System.Windows.Forms.RadioButton();
+			this.radioButton5 = new System.Windows.Forms.RadioButton();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(12, 39);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(220, 130);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Script to obfuscate:";
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(238, 39);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(217, 130);
+			this.richTextBox2.TabIndex = 0;
+			this.richTextBox2.Text = "";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(238, 13);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Obfuscated:";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.checkBox4);
+			this.groupBox1.Controls.Add(this.checkBox3);
+			this.groupBox1.Controls.Add(this.checkBox2);
+			this.groupBox1.Controls.Add(this.checkBox1);
+			this.groupBox1.Location = new System.Drawing.Point(13, 185);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(161, 151);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Options";
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.Location = new System.Drawing.Point(7, 83);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(104, 24);
+			this.checkBox4.TabIndex = 0;
+			this.checkBox4.Text = "Small Output";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.Location = new System.Drawing.Point(7, 62);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(104, 24);
+			this.checkBox3.TabIndex = 0;
+			this.checkBox3.Text = "Anti Dump";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Location = new System.Drawing.Point(7, 41);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(148, 24);
+			this.checkBox2.TabIndex = 0;
+			this.checkBox2.Text = "Encrypt Constants";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(7, 20);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(104, 24);
+			this.checkBox1.TabIndex = 0;
+			this.checkBox1.Text = "Encrypt Strings";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.radioButton7);
+			this.groupBox2.Controls.Add(this.radioButton6);
+			this.groupBox2.Controls.Add(this.radioButton5);
+			this.groupBox2.Controls.Add(this.radioButton4);
+			this.groupBox2.Controls.Add(this.radioButton3);
+			this.groupBox2.Controls.Add(this.radioButton2);
+			this.groupBox2.Controls.Add(this.radioButton1);
+			this.groupBox2.Location = new System.Drawing.Point(180, 185);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(147, 151);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Bytecode";
+			// 
+			// radioButton7
+			// 
+			this.radioButton7.Location = new System.Drawing.Point(6, 123);
+			this.radioButton7.Name = "radioButton7";
+			this.radioButton7.Size = new System.Drawing.Size(104, 19);
+			this.radioButton7.TabIndex = 0;
+			this.radioButton7.Text = "Emoji";
+			this.radioButton7.UseVisualStyleBackColor = true;
+			// 
+			// radioButton6
+			// 
+			this.radioButton6.Location = new System.Drawing.Point(6, 108);
+			this.radioButton6.Name = "radioButton6";
+			this.radioButton6.Size = new System.Drawing.Size(104, 16);
+			this.radioButton6.TabIndex = 0;
+			this.radioButton6.Text = "Chinese";
+			this.radioButton6.UseVisualStyleBackColor = true;
+			// 
+			// radioButton5
+			// 
+			this.radioButton5.Location = new System.Drawing.Point(6, 90);
+			this.radioButton5.Name = "radioButton5";
+			this.radioButton5.Size = new System.Drawing.Size(104, 17);
+			this.radioButton5.TabIndex = 0;
+			this.radioButton5.Text = "Whitespace";
+			this.radioButton5.UseVisualStyleBackColor = true;
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.Location = new System.Drawing.Point(6, 71);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(104, 19);
+			this.radioButton4.TabIndex = 0;
+			this.radioButton4.Text = "Russian";
+			this.radioButton4.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(6, 56);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(104, 17);
+			this.radioButton3.TabIndex = 0;
+			this.radioButton3.Text = "Symbols 1";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(6, 38);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 20);
+			this.radioButton2.TabIndex = 0;
+			this.radioButton2.Text = "Letters 1";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Checked = true;
+			this.radioButton1.Location = new System.Drawing.Point(6, 22);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 18);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Arabic";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.comboBox1);
+			this.groupBox3.Location = new System.Drawing.Point(333, 185);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(129, 151);
+			this.groupBox3.TabIndex = 2;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Platform";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+			"Lua",
+			"Roblox",
+			"CSGO"});
+			this.comboBox1.Location = new System.Drawing.Point(6, 23);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(116, 21);
+			this.comboBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.button1.Location = new System.Drawing.Point(180, 342);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(146, 37);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Obfuscate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(20, 342);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 14);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "API Key:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(20, 358);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(148, 20);
+			this.textBox1.TabIndex = 5;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(470, 387);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.ShowIcon = false;
+			this.Text = "MoonSecAPITest";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MoonSecAPITest/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace MoonSecAPITest
+{
+	
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+		
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			RadioButton[] bytecode = { radioButton1, radioButton2, radioButton3, radioButton4, radioButton5, radioButton6, radioButton7 };
+			int bytecodeindex = -1;
+			for (int i = 0; i < bytecode.Length; i++) {
+				if (bytecode[i].Checked) {
+					bytecodeindex = i;
+					break;
+				}
+			}
+			
+			string script = richTextBox1.Text;
+			string platform = comboBox1.Text;
+			
+			CheckBox[] boxes = { checkBox1, checkBox2, checkBox3, checkBox4 };
+			string[] paramname = { "StringEncryption", "ConstantEncryption", "AntiDump", "SmallOutput" };
+			
+			string options_payload = "";
+			for (int i = 0; i < boxes.Length; i++) {
+				if (boxes[i].Checked) {
+					options_payload += paramname[i] + "+"; // i know, but it doesn't matter if it has + at the very end.
+				}
+			}
+			
+			var request = (HttpWebRequest)WebRequest.Create("https://api.f3d.at/v1/obfuscate.php?key=" + textBox1.Text + "&options=" + options_payload + "&bytecode=" + bytecodeindex + "&platform=" + platform);
+
+
+			byte[] _byteScript = Encoding.UTF8.GetBytes(script);
+			request.Method = "POST";
+			request.ContentType = "text/html charset=utf-8;";
+			request.ContentLength = _byteScript.Length;
+			using (var stream = request.GetRequestStream())
+			{
+    			stream.Write(_byteScript, 0, _byteScript.Length);
+			}
+			
+			
+			try{
+			var response = (HttpWebResponse)request.GetResponse();
+			var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+			richTextBox2.Text = responseString;
+			}catch(WebException ex){ MessageBox.Show(ex.Message); }
+			
+			
+	
+		}
+	}
+}
